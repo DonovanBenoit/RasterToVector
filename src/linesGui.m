1db45d40984108de8183cc21aa9374cbd188ebbf
db <donovan.benoit@gmail.com>
2011-07-25 11:24:50 -0230

    cleanup of the lines gui

--- src/linesGui.m
+++ src/linesGui.m
@@ -22,7 +22,7 @@ function varargout = linesGui( varargin )
 
 % Edit the above text to modify the response to help linesGui
 
-% Last Modified by GUIDE v2.5 23-Jul-2011 19:58:36
+% Last Modified by GUIDE v2.5 25-Jul-2011 10:56:49
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -65,9 +65,6 @@ mMin = str2double( get( handles.minLength, 'String' ) );
 % Gap distance to fill in lines
 global mFill;
 mFill = str2double( get( handles.gapFill, 'String' ) );
-% Number of peks to detect when using hough
-global mMaxP;
-mMaxP = str2double( get( handles.maxPeaks, 'String' ) );
 % Flag to show steps in line detection
 global mShowSteps;
 mShowSteps = 1.0;
@@ -87,7 +84,7 @@ imshow( mImage, 'Parent', handles.axes1 );
 global mBackup;
 mBackup = mImage;
 % Update handles structure
-guidata( hObject, handles );
+%guidata( hObject, handles );
 
 % --- Outputs from this function are returned to the command line.
 function varargout = linesGui_OutputFcn( hObject, eventdata, handles ) 
@@ -109,113 +106,13 @@ imshow( mImage, 'Parent', handles.axes1 );
 
 % --- Executes on button press in linesButton.
 function linesButton_Callback( hObject, eventdata, handles )
+houghLines( hObject, handles );
 
-if get( handles.useHough,'Value' ) == 1
-    houghLines( hObject, handles );
-else
-    set( handles.console, 'String', 'Other' );
-    otherLines( hObject, handles );
-end
-
-function otherLines( hObject, handles )
-global mImage;
-global mShowSteps;
-nColors = 4;
-vertexResolution = 10;
-
-image = rgb2gray( mImage );
-imshow( image, 'Parent', handles.axes1 );
-if mShowSteps == 1
-    pause;
-end
-edges = edge( image, 'canny' );
-imshow( edges, 'Parent', handles.axes1 );
-if mShowSteps == 1
-    pause;
-end
-
-% Reduce the number of colors in the image
-[ img2, map ] = gray2ind( edges, nColors );
-
-for i = 1:nColors
-  layers(i) = { medfilt2( eq( img2 + 1, i ) ) };
-end
-
-subplot(1,2,2);
-
-axis ([0 size(img2,2) -size(img2,1) 0]);
-axis image;
-axis off;
-
-hold on;
-
-% Draw shapes.
-for i=1:nColors
-    
-    % Fill holes.
-    layers{i} = imfill(layers{i},'holes');
-    
-    % Trace edges of continuous blocks of colour. 
-    % Data structure is now:
-    % 'Layered_img' contains cell array of:
-    % ---> 'Layers', which contain cell array of:
-    % ------> 'Shapes' in the layer.
-    layers{i} = bwboundaries(layers{i});
-
-    % Find number of shapes in layer.
-    shapes_in_layer = size(layers{i},1);
-    
-    k=1;
-    
-    while k<=shapes_in_layer
-        
-        % Get number of points in this shape
-        points_in_shape = size(layers{i}{k},1);
-        
-
-        % Percentage of points to keep.
-        points_to_skip = cast( ceil(100/vertexResolution),'uint32');
-       
-        % Reduce number of points in each shape.
-        layers{i}{k} = layers{i}{k}([1:points_to_skip:points_in_shape],[1 2]);
-    
-        % If 'shape' is now a point, remove it.
-
-        if size(layers{i}{k},1) <= 1
-            layers{1,i}{k,1} = [0 0];
-        end
-        k=k+1;
-        
-    end
-    
-    % Find number of shapes in layer.
-    shapes_in_layer = size(layers{i},1);
-    
-    % Plot the shapes as polygons
-    %if(i==2)
-        for k=1:shapes_in_layer
-            p = patch(layers{i}{k}(:,2),-layers{i}{k}(:,1),1);
-
-            if(i == 2)
-                set(p,'FaceColor',[map(i,:)]);
-                %plot(layers{2}{2}(:,2),-layers{2}{2}(:,1),'*')
-                %disp(layers{2}{1})
-            else
-                set(p,'FaceColor',[map(i,:)]);
-            end
-
-            set(p,'EdgeColor','none');
-        end
-   % end    
-end
-
-guidata( hObject, handles );
-
+% Find lines in the image using the Hough Transform Method
 function houghLines( hObject, handles )
 global mImage;
 global mFill;
 global mMin;
-global mMaxPeaks;
 global mShowSteps;
 
 %axis on, axis normal, hold on;
@@ -236,18 +133,17 @@ ylabel( handles.axes1, '\rho' );
 if mShowSteps == 1
     pause;
 end
-peaks = houghpeaks( H, 64 );%, 'threshold', ceil( 0.3 * max( H( : ) ) ) );
+peaks = houghpeaks( H, 500, 'threshold', ceil( 0.3 * max( H( : ) ) ) );
 x = theta( peaks( :, 2 ) ); 
 y = rho( peaks( :, 1 ) );
 plot( handles.axes1, x, y, 's', 'color', 'red' );
 if mShowSteps == 1
     pause;
 end
-lines = houghlines( H, theta, rho, peaks );%, 'FillGap', mFill, 'MinLength', mMin );
+lines = houghlines( H, theta, rho, peaks, 'FillGap', mFill, 'MinLength', mMin );
 imshow( mImage, 'Parent', handles.axes1 );
 hold( handles.axes1 );
 %hold on;
-set( handles.console, 'String', length( lines ) );
 for k = 1:length( lines )
     
     x1 = lines(k).point1(2);
@@ -258,25 +154,10 @@ for k = 1:length( lines )
 end
 hold( handles.axes1 );
 
-% %axes( handles.axes1 );
-% for k = 1:length( lines )
-%    xy = [ lines( k ).point1; lines( k ).point2 ];
-%  %plot( handles.axes1, lines( k ).point1, lines( k ).point2 );
-%  %line( handles.axes1, lines(k) )
-%    %if k == 1
-%    plot( handles.axes1, lines( k ).point1, lines( k ).point2,'LineWidth',2,'Color','green');
-%    %end
-%    % Plot beginnings and ends of lines
-%    plot( handles.axes1, xy( 1,1 ), xy( 1,2 ), 'x', 'LineWidth', 2, 'Color', 'blue');
-%    plot( handles.axes1, xy( 2,1 ), xy( 2,2 ), 'x', 'LineWidth', 2, 'Color', 'blue');
-%    hold( handles.axes1 );
-% end
 guidata( hObject, handles );
    
-% --- Executes on button press in apply.
-function apply_Callback( hObject, eventdata, handles )
-global mImage;
 
+% Set the gap distance to fill
 function gapFill_Callback( hObject, eventdata, handles )
 global mFill;
 
@@ -289,6 +170,7 @@ else
     set( hObject, 'String', mFill );
 end
 
+% Set the minimum length for lines
 function minLength_Callback( hObject, eventdata, handles )
 global mMin;
 
@@ -301,23 +183,7 @@ else
     set( hObject, 'String', mMin );
 end
 
+% Toggle flag to display steps
 function showStepsChecked_Callback( hObject, eventdata, handles )
 global mShowSteps;
 mShowSteps = get( hObject,'Value' );
-
-function maxPeaks_Callback( hObject, eventdata, handles )
-global mMaxPeaks;
-
-max = str2double( get( hObject, 'String' ) );
-if isnan( max ) || ~isreal( max ) 
-    % Restore original value
-    set( hObject, 'String', mMaxPeaks );
-else 
-    mMaxPeaks = max;
-    set( hObject, 'String', mMaxPeaks );
-end
-
-
-% --------------------------------------------------------------------
-function saveMenu_Callback( hObject, eventdata, handles )
-toSVG();
